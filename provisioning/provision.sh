02b3348468bdd2bad1d506e20c0cba588beab4fd
Jimmy Gong <gong.jim@gmail.com>
2016-04-04 22:54:43 -0700

    Updates provisioning script, adds requirements.txt

--- provisioning/provision.sh
+++ provisioning/provision.sh
@@ -1,3 +1,9 @@
 # Using Python v3.4.3
+apt-get install -y python3-pip
+sudo pip3 install virtualenv
+
 echo "alias python=python3" >> /home/vagrant/.bashrc
+echo "alias pip=pip3" >> /home/vagrant/.bashrc
 echo "cd ~/app" >> /home/vagrant/.bashrc
+echo "virtualenv venv" >> /home/vagrant/.bashrc
+echo "source venv/bin/activate" >> /home/vagrant/.bashrc
