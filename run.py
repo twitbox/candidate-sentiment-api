3c1fd9936e2e2e70c6c372b415f65afc5b1bc1f3
Jimmy Gong <gong.jim@gmail.com>
2016-04-05 19:39:47 -0700

    Adds flask server

--- run.py
+++ run.py
@@ -1,1 +1,11 @@
-print(3/2)
+from flask import Flask
+
+app = Flask(__name__)
+
+FLASK_CONFIG = {
+  'DEBUG': True
+}
+app.config.update(FLASK_CONFIG)
+
+if (__name__):
+  app.run(port=8000, host='0.0.0.0')
